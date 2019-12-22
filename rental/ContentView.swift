a12cab5e88ffedca4fbb8adb2da3e6c9c165596a
Dr. Hans-Thomas Nürnberg <nuernberg@me.com>
2019-12-22 17:37:48 +0100

    Initial Commit

---
+++ rental/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rental
+//
+//  Created by Dr. Nürnberg on 22.12.19.
+//  Copyright © 2019 G2323. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
